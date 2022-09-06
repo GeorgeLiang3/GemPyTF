2f207dd79f5c190aa49b3c367443a5bcfc222b5a
GerogeLiang3 <liangzhouji@gmail.com>
2022-09-06 14:54:50 -0500

    fix bug

--- Examples/model2-Anticline.py
+++ Examples/model2-Anticline.py
@@ -4,7 +4,11 @@ sys.path.append('../GP_old/')
 
 import gempy as gp
 
-from gempy.core.tensor.modeltf import ModelTF
+from gempy.core.tensor.modeltf_var import ModelTF
+import tensorflow as tf
+
+tf.config.run_functions_eagerly(True)
+
 
 # %%
 data_path = 'https://raw.githubusercontent.com/cgre-aachen/gempy_data/master/'
@@ -16,7 +20,13 @@ geo_data = gp.create_data( extent=[0, 1000, 0, 1000, 0, 1000], resolution=[50, 5
 
 gp.map_series_to_surfaces(geo_data, {"Strat_Series": ('rock2', 'rock1'), "Basement_Series": ('basement')})
 
+geo_data.add_surface_values([1.0,4.0,2.0])
+geo_data.modify_order_surfaces(2,0)
 
+# %%
+## modify one surface point
+
+geo_data.surface_points.modify_surface_points(21,Z = 600,surface = 'rock2')
 # %%
 ## I will integrate the module into GemPy through Interpolator later
 model = ModelTF(geo_data)
@@ -27,9 +37,9 @@ model.create_tensorflow_graph(gpinput,gradient = False)
 # %%
 model.compute_model()
 # %%
-### 3D plot
+# 3D plot
 gp._plot.plot_3d(model)
 # %%
 gp.plot.plot_section(model, cell_number=15,
-                         direction='y', show_data=True)
+                         direction='y', show_data=True,show_all_data = True,show_boundaries = True)
 # %%
